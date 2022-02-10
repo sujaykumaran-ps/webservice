f8f62d325e4d6f1ad693b342c4ab470d55b596dc
sujaykumaran-ps <palanikumarsankara.s@northeastern.edu>
2022-02-09 22:07:12 -0500

    Changes in Test File

--- test/test.js
+++ test/test.js
@@ -1,26 +1,21 @@
 var supertest = require("supertest");
 var should = require("should");
 
-// This agent refers to PORT where program is runninng.
 
 var server = supertest.agent("http://localhost:3000");
 
-// UNIT test begin
 
-describe("Unit Test",function(){
-
-  // #1 should return home page
+describe("Unit Test",function(){ 
 
   it("should return 200 Status",function(done){
 
-    // calling home page api
     server
     .get("/healthz")
     .expect("Content-type",/json/)
-    .expect(200) // THis is HTTP response
+    .expect(200) 
     .end(function(err,res){
         res.status.should.equal(200);
-        res.type.should.equal("application/json")
+        res.type.should.equal("application/json");
         done();
     });
   });
