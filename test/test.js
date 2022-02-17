e946a03ae1d7c7d7c523338facadf261ee38af58
sujaykumaran-ps <palanikumarsankara.s@northeastern.edu>
2022-02-16 19:20:28 -0500

    Modified README

--- test/test.js
+++ test/test.js
@@ -14,6 +14,8 @@ describe("Unit Test",function(){
     .expect("Content-type",/json/)
     .expect(200) 
     .end(function(err,res){
+        res.status.should.equal(200);
+        res.type.should.equal("application/json");
         done();
     });
   });
