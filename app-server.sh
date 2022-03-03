a3e37a3f4aa54e303e4454b33175c17da3815ddb
sujaykumaran-ps <palanikumarsankara.s@northeastern.edu>
2022-03-03 14:16:46 -0500

    Cahnges to Actions YML file

--- app-server.sh
+++ app-server.sh
@@ -26,11 +26,11 @@ npm -v
 # mysql --connect-expired-password -u root -p$pass -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Jayashree44.';"
 
 sudo yum update -y sudo 
-sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+sudo yum -your install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo amazon-linux-extras install epel -y
 sudo yum -y install mysql-community-server
-sudo systemctl enable --now mysqld
-sudo systemctl status mysqld
+sudo systemctl enable --now mysqld.service
+sudo systemctl status mysqld.service
 pwd=$(sudo grep 'temporary password' /var/log/mysqld.log | rev | cut -d':' -f 1 | rev | xargs) mysql -uroot -p$pwd --connect-expired-password -e "CREATE USER 'ec2-user'@'%' IDENTIFIED BY 'Jayashree44.';" 
 grant all privileges on *.* to 'ec2-user'@'%' with grant option; 
 mysql -uec2-user -pJayashree44. -e "CREATE DATABASE IF NOT EXISTS userdb"
@@ -52,75 +52,3 @@ sudo cp webservice.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable webservice
 sudo systemctl start webservice
-
-
-# npm install -g pm2
-
-# mkdir -p ~/code/app-dist
-# mv /tmp/webservice ~/code/app-dist/webservice
-# cd  ~/code/app-dist/webservice
-# pm2 start app.js
-# pm2 startup systemd
-# pm2 save
-# pm2 list
-
-
-# # In the temp dir, get the node binaries and extract them
-# curl -sLO https://nodejs.org/dist/latest-v14.x/node-$VERSION.tar.gz
-# tar -xzf node-$VERSION.tar.gz
-# mv node-$VERSION /usr/src/node
-
-# # Link the binaries onto the $PATH
-# ln -s /usr/src/node/bin/node /usr/bin/node
-# ln -s /usr/src/node/bin/npm /usr/bin/npm
-# ln -s /usr/src/node/bin/npx /usr/bin/npx
-
-# # Clean the temporary directory
-# rm -r $TMP
-
-# # Create the unix user
-# adduser node
-
-# # Add them to groups
-# usermod -aG sudo node
-# usermod -aG gpio node
-
-# # Create a node-owned folder to put the app in
-# # You call this whatever you like, probably the name of your application
-# mkdir -p /usr/src/webservice
-# chown -R node:node /usr/src/webservice
-
-# # Become our new node user
-# su node
-
-# # Clone the repo
-# git clone git@github.com:the-office-csye6225/webservice.git /usr/src/webservice
-# cd /usr/src/webservice
-
-# # Install the app's production dependencies
-# # There could be trouble here if any of your dependencies require extra binaries, like python
-# # You might need to debug you packages and see what they require a bit
-# npm install 
-
-# SERVICE=webservice
-
-# # Link the service file into place
-# sudo ln -s /usr/src/webservice/webservice.service /lib/systemd/system/webservice.service
-
-# # Reload the daemon so it knows about the new file
-# sudo systemctl daemon-reload
-
-# # Enable our new service
-# sudo systemctl enable $SERVICE
-
-# # Start the service
-# sudo systemctl start $SERVICE
-
-# Install MySQL Server
-# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
-# echo install epel
-# sudo amazon-linux-extras install epel
-# echo install community server
-# sudo yum -y install mysql-community-server
-# sudo systemctl enable --now mysqld
-# systemctl status mysqld
