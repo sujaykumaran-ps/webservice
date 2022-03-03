2aec2ac853ac0e3d5b823f3b4b155c5afb691c16
sujaykumaran-ps <palanikumarsankara.s@northeastern.edu>
2022-03-03 18:00:19 -0500

    Changes to SH sleep

--- app-server.sh
+++ app-server.sh
@@ -68,6 +68,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable webservice
 sudo systemctl start webservice
 npm install pm2 -g
+sleep 15
 sudo pm2 start webservice/app.js
 sudo pm2 startup
 pm2 list
