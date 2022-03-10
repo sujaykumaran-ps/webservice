3d8a7ac12154ddf5045c8aec4ef377f8e3536a33
sujaykumaran-ps <palanikumarsankara.s@northeastern.edu>
2022-03-10 07:14:48 -0500

    Included RDS Config

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -19,8 +19,6 @@ const fs = require("fs");
 
 var AWS = require("aws-sdk");
 AWS.config.update({ 
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
   region: "us-east-1" });
 s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
